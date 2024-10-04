8217440bb966221cd4b566549aac77548963f980
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2024-10-04 09:20:30 -0400

    f

---
+++ index.js
@@ -0,0 +1,22 @@
+// const fs = require('fs');
+// const path = require('path');
+const express = require('express');
+// const fetch = require('node-fetch');
+// const { obfuscate } = require('javascript-obfuscator');
+// const showdown = require('showdown');
+
+const server = express();
+const PORT = 3000;
+
+server.use(express.static('public'));
+
+server.get('*', (req, res) => {
+  res.status(404).sendFile(`${__dirname}/404.html`);
+});
+
+if (process.env && process.env.replit !== '') {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+    online = true;
+  });
+}
